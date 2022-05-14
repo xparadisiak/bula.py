6702832ea6246c211f6043d3ba2c7d857ce93c7f
xparadisiak <85847484+xparadisiak@users.noreply.github.com>
2022-05-14 20:47:15 +0200

    Create ciekawostki.py

---
+++ ciekawostki.py
@@ -0,0 +1,4 @@
+ciekawostki = {
+    "Potrzeba ponad 60 tysięcy lat, aby obejrzeć wszystkie filmiki zamieszczone obecnie na Youtube.",
+    "Krew może służyć podczas gotowania jako zamiennik jajek."
+}
