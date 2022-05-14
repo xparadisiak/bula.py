ec805793064cbf8a8cbe6141eda2934683ed3950
xparadisiak <85847484+xparadisiak@users.noreply.github.com>
2022-05-14 22:29:41 +0200

    Update ciekawostki.py
    Database for "fun fact" function used in main project

--- ciekawostki.py
+++ ciekawostki.py
@@ -1,4 +1,27 @@
-ciekawostki = {
+import random 
+
+ciekawostki = [
     "Potrzeba ponad 60 tysięcy lat, aby obejrzeć wszystkie filmiki zamieszczone obecnie na Youtube.",
-    "Krew może służyć podczas gotowania jako zamiennik jajek."
-}
+    "Krew może służyć podczas gotowania jako zamiennik jajek.",
+    "Dwie trzecie populacji na świecie nigdy nie widziało śniegu.",
+    "Hymn Królestwa Hiszpanii nie ma słów.",
+    "Howard Hughes – amerykański miliarder i konstruktor lotniczy kupił kasyno tylko po to, by usunąć z niego neon, który widział ze swojego okna.",
+    "Jeden dzień na Ziemi trwa dłużej niż jeden rok na Wenus.",
+    "Wydry podczas snu trzymają się nawzajem za łapki, żeby nie odpłynąć od siebie.",
+    "Koliber waży mniej niż moneta 50 groszy.",
+    "Spożycie wysokoprocentowych alkoholi w Korei Północnej jest wyższe niż w Rosji.",
+    "Istnieje więcej możliwości ruchów w szachach niż atomów we wszechświecie.",
+    "Na twoim ciele znajduje się więcej organizmów żywych niż ludzi na ziemi.",
+    "Przeciętny wysportowany człowiek przechodzi w swoim życiu dystans równy pięciu pokrążeniom ziemi.",
+    "Aby wypić całą krew z organizmu człowieka potrzeba by około miliona komarów.",
+    "W SIngapurze zabronione jest żucie gumy. W Singapurze powszechne było przylepianie zużytych gum do aut, pociągów i innych obiektów na stacjach metra – gdzie przylepiane gumy powodowały blokowanie drzwi pociągów i inne zakłócenia ruchu.",
+    "Ze względu na makaron, który we Włoszech jedzony jest bardzo często najwcześniej zastosowanie znalazł widelec niż w jakimkolwiek innym kraju."
+
+
+
+
+
+]
+
+losowa_ciekawostka = random.choice(ciekawostki)
+print(losowa_ciekawostka)
